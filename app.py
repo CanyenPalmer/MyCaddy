5b7fc2c2aa97c1f3be5b63f0e32d743215f680ab
Canyen Palmer <Canyen2019@gmail.com>
2025-08-03 13:18:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,8 @@ def index():
 
     return render_template('index.html', result=result, summary=summary)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
