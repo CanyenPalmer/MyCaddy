ae95f92e47977760cf978db82d5dc118fb10bc20
Canyen Palmer <Canyen2019@gmail.com>
2025-08-03 12:58:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def index():
     return render_template('index.html', result=result, summary=summary)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=10000)
+
